3b1de71319ac2c097a7d3a8e4d6d808460d9aa19
Kingstimusprime <kingston.james7710@gmail.com>
2023-05-09 14:37:47 -0400

    beta v1

--- public/main.js
+++ public/main.js
@@ -13,45 +13,68 @@ var velocity = {x: 0, y: 1};
 var objects = [];
 var currKey = {};
 var onGround;
+// Images
 var banditImage = document.querySelector("#LBandit");
-var redImage = document.querySelector("#red");
-var tileMap = document.querySelector("#map")
+const redImage = document.querySelector("#red");
+const tileMap = document.querySelector("#map")
+const cityTileMap = document.querySelector("#cityMap")
+const goldTiles = document.querySelector("#gold");
 var frame = 0;
 var flip = -1; // local version of flipped var sent to server
 var attacking = false; // local variable that keeps track of wether or not the attack has finished;
 var tSinceA = 0; // time since user attacked
 var tSinceHurt = 0; // when player was last hit
 var scroll = {x: 0, y: 0}; // amount game is scrolled by
-const debug = true;
+const debug = false;
 var dead = false; // is player dead
 const maxSpeed = 13;
 const gravity = 5;
-const jumpHeight = 42;
+const jumpHeight = 46;
+// all of the gold on the map
+var gold = {};
 var beat = new Audio('./public/assets/heart.wav');
 beat.loop = true;
 
 var map = [];
 // client-side
 socket.on("new_player", (arg) => {
-    console.log(arg.player_id);
     players[arg.player_id] = arg;
-    console.log(players[arg.player_id])
   });
 
 socket.on("actualPlayers", (data) => {
     players = data.p;
     uid = data.uid;
-    fetch("./public/map.json").then((response) => response.json()).then((json) => {
-      console.log(json)
+    gold = data.gold;
+    for(const g in gold){
+      gold[g].width = players[uid].width * 0.5;
+      gold[g].height = players[uid].width * 0.5;
+    }
+    fetch("./public/newMap.json").then((response) => response.json()).then((json) => {
       let map = json["layers"][0].data;
-      createObjects(32, map, 100);
+      createObjects(32, map, 175, "envir");
+      map = json["layers"][1].data;
+      createObjects(32, map, 175, "city");
       drawPlayers();
     });
 
 });
 
 socket.on("player_disconnect", (id) => {
+    const quitP = players[id]
     delete players[id];
+    if(uid == Object.keys(players)[0]){
+      for(let i = 0; i < quitP.gold; i++){
+        console.log(uid + i)
+        const t = Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01;
+        socket.emit("addGold", {id: uid + i, gold: {
+          "x": quitP.x + (Math.floor(Math.random() * quitP.width * 2) - quitP.width) + quitP.width/2,
+          "y": quitP.y + 72,
+          "w": 16,
+          "h": 16,
+          type: t,
+        }})
+      }
+    }
 });
 
 socket.on("changeAnim", (animData) => {
@@ -72,6 +95,26 @@ socket.on("setPos", (data) => {
 })
 
 
+socket.on("CollectGold", function(goldData) {
+  players[goldData.player].gold += 1;
+  console.log(goldData);
+  delete gold[goldData.id];
+})
+
+socket.on("resetPlayer", function(data) {
+  players[data.id] = data.player;
+  if(data.id == uid){
+    dead = false;
+    scroll.x = 0;
+    scroll.y = 0;
+  }
+})
+
+socket.on("addGold", function(goldData) {
+  gold[goldData.id] = goldData.gold;
+  gold[goldData.id].height = players[uid].width * 0.5;
+  gold[goldData.id].width = players[uid].width * 0.5;
+})
 
 socket.on("damagePlayer" , function(HurtData) {
   if(HurtData.id == uid){
@@ -95,15 +138,15 @@ function getRndColor() {
   return 'rgb(' + r + ',' + g + ',' + b + ')';
 }
 
-function createObjects(size, objs, width){
+function createObjects(size, objs, width, type){
   for (let i = 0; i < objs.length; i++) {
     const element = objs[i];
     if(element != -1){
       const c = getRndColor(); 
-      if(element == 0.19){
-        objects.push({x: (i%width) * size, y: Math.floor(i/width)  * size, width: size, height: size, c: c, tile: element, type: "water"});
+      if(element == 0.19 && type == "envir"){
+        objects.push({x: (i%width) * size, y: Math.floor(i/width)  * size, width: size, height: size, c: c, tile: element, type: "water", art: "envir"});
       }else{
-        objects.push({x: (i%width) * size, y: Math.floor(i/width)  * size, width: size, height: size, c: c, tile: element, type: "solid"});
+        objects.push({x: (i%width) * size, y: Math.floor(i/width)  * size, width: size, height: size, c: c, tile: element, type: "solid", art: type});
       }
     }
   }
@@ -139,12 +182,17 @@ function attack(attackData, p){
       attackData.x += p.x;
     }
 
+  }else{
+    if(p.flipped == 1){
+      attackData.x = p.x - attackData.x + p.width/2;
+    }else{
+      attackData.x += p.x;
+    }
+    attackData.y += p.y;
   }
   for(play in players){
     if(play != uid){
-      console.log(players[play])
       if(boxCollision(attackData, players[play])){
-        console.log("KNOCKOUT");
         socket.emit("damagePlayer", { id:players[play].player_id, health: 10});
         return true;
       }
@@ -153,7 +201,11 @@ function attack(attackData, p){
   return false;
 }
 
-function update(){
+function update()
+{
+  if(dead && tSinceHurt > 120){
+    socket.emit("resetPlayer", uid);
+  }
   frame += 1;
   tSinceA += 1;
   tSinceHurt += 1;
@@ -164,6 +216,7 @@ function update(){
   }
   if(players[uid].health <= 0){
     if(!dead){
+
       socket.emit("changeAnim", {color: "yellow", anim: 3, frame: frame%7, flipped: flip});
     }else{
       socket.emit("changeAnim", {color: "yellow", anim: 4, frame: 4, flipped: flip});
@@ -171,6 +224,18 @@ function update(){
 
     if(frame%7 == 0 && frame > 0){
       dead = true;
+      for(let i = 0; i < players[uid].gold; i++){
+        console.log(uid + i)
+        const t = Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01;
+        socket.emit("addGold", {id: uid + i, gold: {
+          "x": players[uid].x + (Math.floor(Math.random() * players[uid].width * 2) - players[uid].width) + players[uid].width/2,
+          "y": players[uid].y + 72,
+          "w": 16,
+          "h": 16,
+          type: t,
+        }})
+      }
+      players[uid].gold = 0;
     }
   }else if(!attacking){
     if(!onGround){
@@ -193,36 +258,52 @@ function update(){
     }
     socket.emit("changeAnim", {color: "green", anim: 2, frame: frame + 2%5, flipped: flip});
   }
-
-  if(currKey["ArrowUp"]){
-    // Move up by subtracting speed from top
-    if(onGround){
-      velocity.y -= jumpHeight;
+  if(players[uid].y > 1600){
+    socket.emit("resetPlayer", uid);
+    for(let i = 0; i < players[uid].gold; i++){
+      console.log(uid + i)
+      const t = Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01;
+      socket.emit("addGold", {id: uid + i, gold: {
+        "x":453 + (Math.floor(Math.random() * players[uid].width * 2) - players[uid].width) + players[uid].width/2,
+        "y": 968 + 72,
+        "w": 16,
+        "h": 16,
+        type: t,
+      }})
     }
+    players[uid].gold = 0;
   }
-  if(currKey["ArrowDown"]){
-    // Move down by adding speed to top
-    velocity.y += 2;
-  }else if(currKey["ArrowLeft"]){
-    // Move left by subtracting speed from left
-    velocity.x = maxSpeed * -1;
-  }else{
-    velocity.x = 0;
-  }
-  if(currKey["ArrowRight"]){
-    // Move right by adding speed to left
-    velocity.x = maxSpeed;
-  }
-
-  if(currKey[" "]){
-    if(onGround && tSinceA > 8){
-
-      tSinceA = 0;
-      frame = 0;
-      attacking = true;
-
+  if(!dead){
+    if(currKey["ArrowUp"]){
+      // Move up by subtracting speed from top
+      if(onGround){
+        velocity.y = jumpHeight * -1;
+      }
     }
-
+    if(currKey["ArrowDown"]){
+      // Move down by adding speed to top
+      velocity.y += 2;
+    }else if(currKey["ArrowLeft"]){
+      // Move left by subtracting speed from left
+      velocity.x = maxSpeed * -1;
+    }else{
+      velocity.x = 0;
+    }
+    if(currKey["ArrowRight"]){
+      // Move right by adding speed to left
+      velocity.x = maxSpeed;
+    }
+  
+    if(currKey[" "]){
+      if(onGround && tSinceA > 8){
+  
+        tSinceA = 0;
+        frame = 0;
+        attacking = true;
+  
+      }
+  
+    }
   }
   const cPos = players[uid].x; // keep track of the real pos of player 
   const yPos = players[uid].y;
@@ -257,10 +338,13 @@ function drawPlayer(player) {
     ctx.fillRect(player.x + scroll.x, player.y + scroll.y, player.width, player.height);
   }
   if(!dead){
-    drawHealthBar(player, {x: player.width * 0.07, y: player.height * 0.14}, "green");
+    drawHealthBar(player, {x: player.width * 0.07, y: -player.height * 0.1}, "green");
+    ctx.fillStyle = "gold";
+    ctx.font = "18px Helvetica";
+    ctx.fillText(player.gold, player.x + player.width * 0.5 + scroll.x, player.y +  10 + scroll.y, player.width);
   }
   ctx.scale(player.flipped, 1);
-  ctx.drawImage(banditImage, 48 * player.frame, 48 * player.anim, 48, 48, player.x * player.flipped + ((-18  + scroll.x) * player.flipped), player.y + scroll.y, 144 * player.flipped, 144);
+  ctx.drawImage(banditImage, 48 * player.frame, 48 * player.anim, 48, 48, player.x * player.flipped + ((-18  + scroll.x) * player.flipped), (player.y + scroll.y ) - 20, 144 * player.flipped, 144);
   // Restore the context state
   ctx.restore();
 }
@@ -270,20 +354,25 @@ function playerCollides(p, objs){
   onGround = false;
   cp.y += velocity.y;
   for(const obj in objs){
-    if(boxCollision(cp, objs[obj]) && !onGround){
-
-      if(velocity.y > 0){
+    if(boxCollision(cp, objs[obj])){
+      if(objs[obj].type != "water"){
+        if(velocity.y > 0){
           onGround = true;
-          if( objs[obj].type != "water"){
-            velocity.y = 0;
-            players[uid].y = objs[obj].y - p.height - 0.01;
-            socket.emit("setPos", {x:  players[uid].x + velocity.x, y: objs[obj].y - p.height - 0.01});
-          }
-      }
-      if(velocity.y < 0 && objs[obj].type != "water"){
-        velocity.y = 0;
-        players[uid].y = objs[obj].y + objs[obj].height + 0.01;
-        socket.emit("setPos", {x:  players[uid].x, y: objs[obj].y + objs[obj].height - 0.01});
+          velocity.y = 0;
+          players[uid].y = objs[obj].y - p.height - 0.01;
+          socket.emit("setPos", {x:  players[uid].x + velocity.x, y: objs[obj].y - p.height - 0.01});
+        }
+        if(velocity.y < 0){
+          velocity.y = 0;
+          onGround  = false;
+          players[uid].y = objs[obj].y + objs[obj].height + 0.01;
+          socket.emit("setPos", {x:  players[uid].x, y: objs[obj].y + objs[obj].height - 0.01});
+          velocity.y = 4;
+          velocity.x = 0;
+          break;
+        }
+      }else{
+        onGround  = true;
       }
     }
 
@@ -292,22 +381,30 @@ function playerCollides(p, objs){
   cp.x += velocity.x;
 for(const obj in objs){
   if(boxCollision(players[uid], objs[obj])){
-    if(velocity.x > 0 && objs[obj].type != "water"){
-      velocity.x = 0;
-      socket.emit("setPos", {x: objs[obj].x - p.width - .01, y:players[uid].y});
-      players[uid].x =  objs[obj].x - p.width - 0.01;
+    if(objs[obj].type != "water"){
+      if(velocity.x > 0){
+        velocity.x = 0;
+        socket.emit("setPos", {x: objs[obj].x - p.width - .01, y:players[uid].y});
+        players[uid].x =  objs[obj].x - p.width - 0.01;
+      }
+      if(velocity.x < 0){
+        velocity.x = 0;
+        socket.emit("setPos", {x: objs[obj].x + objs[obj].width + 0.01, y:players[uid].y});
+        players[uid].x =  objs[obj].x + objs[obj].width + 0.01;
+      }
+      return
     }
-    if(velocity.x < 0 && objs[obj].type != "water"){
-      velocity.x = 0;
-      socket.emit("setPos", {x: objs[obj].x + objs[obj].width + 0.01, y:players[uid].y});
-      players[uid].x =  objs[obj].x + objs[obj].width + 0.01;
-    }
-    return
   }
 }  
 
 }
 
+function goldCollides(gId){
+  if(boxCollision(players[uid], gold[gId])){
+    console.log("MINE MINE MINE MINE");
+    socket.emit("CollectGold", {"id": gId, "player": uid});
+  }
+}
 
 function drawPlayers() {
   update();
@@ -316,13 +413,10 @@ function drawPlayers() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 
-  velocity.y += gravity;
-
-
+  velocity.y += gravity;  
+  socket.emit("player_moved", {x: velocity.x, y: velocity.y})
   playerCollides(players[uid], objects);
-
   
-  socket.emit("player_moved", {x: velocity.x , y: velocity.y});
 
 
 
@@ -332,24 +426,43 @@ function drawPlayers() {
       ctx.fillRect(objects[objId].x + scroll.x, objects[objId].y + scroll.y, objects[objId].width, objects[objId].height)
     }
     let tileY = objects[objId].tile;
+    if(Math.floor(tileY) == tileY){
+      tileY = 0;
+    }else {
+      if((tileY + "").split(".")[1].length == 1){
+        tileY = Math.round(tileY%1 * 10)
+      }else{
+        tileY = Math.round(tileY%1 * 100)
+      }
+      if(objects[objId].art == "envir"){
+        ctx.drawImage(tileMap, 16 *  Math.floor(objects[objId].tile), tileY * 16, 16, 16, objects[objId].x + scroll.x, objects[objId].y + scroll.y, objects[objId].width, objects[objId].height);
+      }else{
+        ctx.drawImage(cityTileMap, 16 *  Math.floor(objects[objId].tile), tileY * 16, 16, 16, objects[objId].x + scroll.x, objects[objId].y + scroll.y, objects[objId].width, objects[objId].height);
+      }
+    }
+  }
+  for (const playerId in players) {
+    // Draw each player
+    drawPlayer(players[playerId]);
+    if(playerId == uid){
+      if(players[uid].health/players[uid].maxHealth <= 0.35){
+        ctx.drawImage(redImage, 0, 0, canvas.width, canvas.height)
+      }
+    }
+  }
+  for (const g in gold){
+
+    let tileY = gold[g].type;
     if((tileY + "").split(".")[1].length == 1){
       tileY = Math.round(tileY%1 * 10)
     }else{
       tileY = Math.round(tileY%1 * 100)
     }
-    console.log(tileY)
-    ctx.drawImage(tileMap, 16 *  Math.floor(objects[objId].tile), tileY * 16, 16, 16, objects[objId].x + scroll.x, objects[objId].y + scroll.y, objects[objId].width, objects[objId].height);
-  }
-  for (const playerId in players) {
-    // Draw each player
-    drawPlayer(players[playerId]);
-    if(players[playerId] == uid){
-      if((players[uid].health - HurtData.health)/players[uid].maxHealth <= 0.35){
-        ctx.drawImage(redImage, 0, 0, canvas.width, canvas.height)
-      }
+    if(players[uid].health > 0){
+      goldCollides(g);
     }
+    ctx.drawImage(goldTiles, gold[g].w * Math.floor(gold[g].type), gold[g].w * tileY, gold[g].w, gold[g].h, gold[g].x + scroll.x, gold[g].y + scroll.y, players[uid].width * 0.5,players[uid].width * 0.5);
   }
-
   setTimeout(drawPlayers,100);
   
 }
--- server.js
+++ server.js
@@ -12,6 +12,85 @@ var port = 8080;
 
 var players = {};
 
+var gold = {
+  "someG": {
+    "x": 2117.99,
+    "y": 1151.99,
+    "w": 16,
+    "h": 16,
+    type: 1.13,
+  },
+  "g1": {
+    "x": 50,
+    "y": 1039.99,
+    "w": 16,
+    "h": 16,
+    type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01,
+  },
+  "g2": {
+    "x": 1029,
+    "y": 1039,
+    "w": 16,
+    "h": 16,
+    type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01,
+  },
+  "g3": {
+    "x": 1899,
+    "y": 943,
+    "w": 16,
+    "h": 16,
+    type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01,
+  },
+  "g4": {
+    "x": 2528,
+    "y": 1072,
+    "w": 16,
+    "h": 16,
+    type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01,
+  },
+  "g5": {
+    "x": 2949,
+    "y": 1072,
+    "w": 16,
+    "h": 16,
+    type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01,
+  },
+  "g6":  {x: 192.01, y: 1519.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g7": {x: 688.99, y: 1519.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g8": {x: 1481.99, y: 1551.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g9": {x: 2405.99, y: 1519.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g10": {x: 2816.01, y: 1519.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g11": {x: 3825.99, y: 1487.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g12": {x: 3994.99, y: 1487.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g13": {x: 4189.99, y: 1487.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g14": {x: 4189.99, y: 1397.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g15": {x: 4189.99, y: 1307.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g16": {x: 4645.99, y: 1455.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g17": {x: 3520.01, y: 975.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g18": {x: 3570.01, y: 975.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  'g19': {x: 3610.01, y: 975.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g20": {x: 5221.99, y: 975.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g21": {x: 4768.01, y: 975.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g22": {x: 5195.99, y: 719.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g23": {x: 4421.01, y: 879.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g24": {x: 3675.01, y: 783.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g25": {x: 3520.01, y: 783.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g26": {x: 3520.01, y: 527.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g27": {x: 3675.01, y: 527.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g28": {x: 4000.01, y: 751.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g29": {x: 3968.01, y: 559.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},  
+  "g30": {x: 3571.01, y: 783.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g31": {x: 3520.01, y: 239.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g32": {x: 3520.01, y: 399.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g33": {x: 4367.99, y: 207.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g34": {x: 4549.99, y: 207.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g35": {x: 4770.99, y: 207.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g36": {x: 5211.99, y: 367.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g37": {x: 5221.99, y: 207.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g38": {x: 4906.99, y: 495.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+  "g39": {x: 5221.99, y: 527.99, w: 16, h: 16, type: Math.floor(Math.random() * (15-4+1)+4) + Math.floor(Math.random() * (13-4+1) + 4) * 0.01},
+};
+
 // instancing
 
 var app = express(); 
@@ -47,16 +126,17 @@ io.on("connection", function(socket) {
     x: 50,
     y: 450,
     width: 90,
-    height: 144,
+    height: 120,
     color: "black",
     anim: 0,
     frame: 0, 
     flipped: -1,
     health: 40,
     maxHealth: 40,
+    gold: 0,
     };
 
-  socket.emit("actualPlayers", {"p": players, "uid": socket.id}); //sends info back to that socket and not to all the other sockets
+  socket.emit("actualPlayers", {"p": players, "uid": socket.id, "gold": gold}); //sends info back to that socket and not to all the other sockets
   socket.broadcast.emit("new_player", players[socket.id]);
 
   socket.on("player_moved", function(movement_data) {
@@ -95,8 +175,51 @@ io.on("connection", function(socket) {
     }
   })
 
+  socket.on("addGold", function(goldData){
+    io.emit("addGold", goldData);
+    gold[goldData.id] = goldData.gold;
+  })
+
   socket.on("new_bullet", function(bullet_data){
     socket.broadcast.emit("new_bullet", bullet_data);
+  })
+
+  socket.on("resetPlayer", function(pid) {
+    io.emit("resetPlayer", {id: pid, player:  {
+      player_id: pid,
+      x: 50,
+      y: 450,
+      width: 90,
+      height: 120,
+      color: "black",
+      anim: 0,
+      frame: 0, 
+      flipped: -1,
+      health: 40,
+      maxHealth: 40,
+      gold: 0,
+      }})
+    players[pid] =  {
+      player_id: pid,
+      x: 50,
+      y: 450,
+      width: 90,
+      height: 120,
+      color: "black",
+      anim: 0,
+      frame: 0, 
+      flipped: -1,
+      health: 40,
+      maxHealth: 40,
+      gold: 0,
+      };;
+  })
+
+  socket.on("CollectGold", function(goldData){
+    io.emit("CollectGold", goldData);
+    console.log(goldData)
+    players[goldData.player].gold += 1;
+    delete gold[goldData.id];
   })
 
   socket.on("disconnect", function() {
